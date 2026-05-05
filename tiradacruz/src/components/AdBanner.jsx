343b21416be2169c56c2a0f624d95a834cb332e6
EstebanAleart <esteban.aleart@gmail.com>
2026-05-05 12:15:41 +0200

    fix: sacar EffectiveGateCPM, mantener PropellerAds push
    - Elimina los 2 scripts de EffectiveGateCPM de layout.js y AdBanner.jsx
    - AdBanner.jsx queda como placeholder para AdSense futuro
    - sw.js intacto (PropellerAds push notifications zona 10658487)

--- tiradacruz/src/components/AdBanner.jsx
+++ tiradacruz/src/components/AdBanner.jsx
@@ -1,17 +1,4 @@
-"use client"
-
-import Script from "next/script"
-
+// Espacio reservado para ads futuros (AdSense cuando el tráfico lo justifique)
 export default function AdBanner() {
-  return (
-    <div className="flex justify-center my-6">
-      <Script
-        async
-        data-cfasync="false"
-        src="https://pl28802401.effectivegatecpm.com/66767c8fc0dea2105b822768e6a80f04/invoke.js"
-        strategy="afterInteractive"
-      />
-      <div id="container-66767c8fc0dea2105b822768e6a80f04" />
-    </div>
-  )
+  return null
 }
